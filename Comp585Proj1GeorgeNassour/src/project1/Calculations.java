924cea35489f1d38a65f7c21fffea2dd1c79152e
George Nassour <gnasour@gmail.com>
2018-02-11 21:51:01 -0800

    new logic for queue

--- Comp585Proj1GeorgeNassour/src/project1/Calculations.java
+++ Comp585Proj1GeorgeNassour/src/project1/Calculations.java
@@ -4,81 +4,80 @@ package project1;
  * Created by Gaming on 2/11/2018.
  * Handles the calculations input by the user and when "=" is pressed, returns the result to be printed by the text area
  */
+import javax.swing.*;
 import java.lang.Math;
 import java.util.ArrayDeque;
-import java.util.Iterator;
+import javax.swing.JOptionPane;
 
 public class Calculations {
 
     //Calculator acts as a queue
     private static ArrayDeque<String>calculatorQueue = new ArrayDeque<String>();
+    //The GUI copy to reproduce on the text area
+    private static ArrayDeque<String>calculatorQueueGUI;
 
     //Intermediary variables to calculate the expressions entered from GUI
     private static String number1="", number2="";
     private static String operator="";
-    private static String nextInput="";
     private static double result = 0;
 
+
     //Add commands to the queue
-    protected static void addCommand(String calculatorCommand){
-        calculatorQueue.add(calculatorCommand);
-    }
+    protected static void addCommand(String calculatorCommand){calculatorQueue.add(calculatorCommand);}
 
-    //Find the result of the inputs after the user has clicked "="
     protected static void computeResults(){
+        calculatorQueueGUI = calculatorQueue;
+        while(!calculatorQueue.isEmpty()){
+            if(calculatorQueue.peek().equals("+")||calculatorQueue.peek().equals("-")||calculatorQueue.peek().equals("*")||calculatorQueue.peek().equals("/")||calculatorQueue.peek().equals("%")){
+                operator = calculatorQueue.pop();
+                if(!calculatorQueue.isEmpty()){
+                    while(!calculatorQueue.isEmpty()){
+                        try{
+                        if(calculatorQueue.peek().equals("+")||calculatorQueue.peek().equals("-")||calculatorQueue.peek().equals("*")||calculatorQueue.peek().equals("/")||calculatorQueue.peek().equals("%")){
+                            switch(operator){
+                                case "+":
+                                    result = Double.parseDouble(number1) + Double.parseDouble(number2);
+                                    number1 = String.valueOf(result);
+                                    number2 = "";
+                                    break;
+                                case "-":
+                                    result = Double.parseDouble(number1) - Double.parseDouble(number2);
+                                    break;
+                                case "*":
+                                    result = Double.parseDouble(number1) * Double.parseDouble(number2);
+                                    break;
+                                case "/":
+                                    result = Double.parseDouble(number1) / Double.parseDouble(number2);
+                                    break;
+                                case "%":
+                                    result = Double.parseDouble(number1) % Double.parseDouble(number2);
+                                    break;
+                            }
+                        }
+                        else if(calculatorQueue.peek().equals("=")){
+                            result += Double.parseDouble(number1) + Double.parseDouble(number2);
+                            returnResults();
+                        }
+                        else
+                            number2 += calculatorQueue.pop();
+                    }catch(NumberFormatException nfe){
+                            JOptionPane.showMessageDialog(null, "Input a valid sequence to compute.");
+                            resetVariables();
+                        }
+                    }
 
-        Iterator calcInputs = calculatorQueue.iterator();
+                }else{
+                    JOptionPane.showMessageDialog(null, "Enter a valid number after the operator");
+                }
 
-        while(calcInputs.hasNext()){
-
-            nextInput = (String) calcInputs.next();
-            if(nextInput.equals("+") || nextInput.equals("-")|| nextInput.equals("*")|| nextInput.equals("/")|| nextInput.equals("%")){
-
-                operator = nextInput;
-                while(calcInputs.hasNext()){
-
-                    nextInput = (String) calcInputs.next();
-                    if(nextInput.equals("+") || nextInput.equals("-")|| nextInput.equals("*")|| nextInput.equals("/")|| nextInput.equals("%")){
-
-                    }//finding an operand in an operand
-                    else if(nextInput.equals("=")){
-
-                        switch(operator) {
-                            case "+":
-                                result = Double.parseDouble(number1) + Double.parseDouble(number2);
-                                returnResults();
-                                break;
-                            case "-":
-                                result = Double.parseDouble(number1) - Double.parseDouble(number2);
-                                returnResults();
-                                break;
-                            case "*":
-                                result = Double.parseDouble(number1) * Double.parseDouble(number2);
-                                returnResults();
-                                break;
-                            case "/":
-                                result = Double.parseDouble(number1) / Double.parseDouble(number2);
-                                returnResults();
-                                break;
-                            case "%":
-                                result = Double.parseDouble(number1) % Double.parseDouble(number2);
-                                returnResults();
-                                break;
-                        }//cases
-                    }//ending the loop with =
-                    else{
-                        number2 += nextInput;
-                    }//getting numbers for number2
-
-                }//second while for number2
-
-            }//finding operands in first if
-            else{
-                number1 += nextInput;
-            }//if for number1
-
-        }//first while for number1
-
+            }
+            else if(calculatorQueue.peek().equals("=")){
+                result = Double.parseDouble(number1);
+                returnResults();
+            }
+            else
+                number1 += calculatorQueue.pop();
+        }
     }
 
 
@@ -91,5 +90,11 @@ public class Calculations {
         calculatorQueue.clear();
         result = 0;
         number1 = number2 ="";
+    }
+    private static void resetVariables(){
+        calculatorQueue.clear();
+        calculatorQueueGUI.clear();
+        number1 = number2 = "";
+        result = 0;
     }
 }
