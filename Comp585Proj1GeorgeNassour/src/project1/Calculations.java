012c5310b0dde24e7c6c7dda96f550b9ee5889f0
George Nassour <gnasour@gmail.com>
2018-02-11 19:51:13 -0800

    fixed bugs on calculations

--- Comp585Proj1GeorgeNassour/src/project1/Calculations.java
+++ Comp585Proj1GeorgeNassour/src/project1/Calculations.java
@@ -19,48 +19,77 @@ public class Calculations {
     private static String nextInput="";
     private static double result = 0;
 
+    //Add commands to the queue
     protected static void addCommand(String calculatorCommand){
         calculatorQueue.add(calculatorCommand);
     }
 
+    //Find the result of the inputs after the user has clicked "="
     protected static void computeResults(){
+
         Iterator calcInputs = calculatorQueue.iterator();
+
         while(calcInputs.hasNext()){
+
             nextInput = (String) calcInputs.next();
             if(nextInput.equals("+") || nextInput.equals("-")|| nextInput.equals("*")|| nextInput.equals("/")|| nextInput.equals("%")){
+
                 operator = nextInput;
                 while(calcInputs.hasNext()){
+
                     nextInput = (String) calcInputs.next();
                     if(nextInput.equals("+") || nextInput.equals("-")|| nextInput.equals("*")|| nextInput.equals("/")|| nextInput.equals("%")){
 
-                    }
+                    }//finding an operand in an operand
                     else if(nextInput.equals("=")){
-                        result = Double.parseDouble(number1) + Double.parseDouble(number2);
-                        returnResults();
-                    }
+
+                        switch(operator) {
+                            case "+":
+                                result = Double.parseDouble(number1) + Double.parseDouble(number2);
+                                returnResults();
+                                break;
+                            case "-":
+                                result = Double.parseDouble(number1) - Double.parseDouble(number2);
+                                returnResults();
+                                break;
+                            case "*":
+                                result = Double.parseDouble(number1) * Double.parseDouble(number2);
+                                returnResults();
+                                break;
+                            case "/":
+                                result = Double.parseDouble(number1) / Double.parseDouble(number2);
+                                returnResults();
+                                break;
+                            case "%":
+                                result = Double.parseDouble(number1) % Double.parseDouble(number2);
+                                returnResults();
+                                break;
+                        }//cases
+                    }//ending the loop with =
                     else{
                         number2 += nextInput;
-                    }
-                }
+                    }//getting numbers for number2
 
+                }//second while for number2
 
-            }
+            }//finding operands in first if
             else{
                 number1 += nextInput;
-            }
+            }//if for number1
 
-        }
+        }//first while for number1
 
     }
 
 
 
-
+    //Returning the results and clearing the variables
     private static void returnResults(){
         if(Math.floor(result) == result){
             System.out.println((int) result);
         }
         calculatorQueue.clear();
         result = 0;
+        number1 = number2 ="";
     }
 }
